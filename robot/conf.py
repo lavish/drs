41fd371428b1501821bed5152a050b701a3decdc
Marco Squarcina <squarcina@dais.unive.it>
2015-01-24 19:56:28 +0100

    Ported the core program from python-ev3 to the new ev3dev python library

--- robot/conf.py
+++ robot/conf.py
@@ -1,19 +1,36 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from enum import Enum
+
+# [CHANGE ME]
+# set this value to reflect the id of your robot. Allowed values are [0..3]
+my_robot_id = 0
+
 # number of ir samples to consider for median calculation
 n_ir_samples = 15
 
 # constant for the line following task: the lower the smoother
-proportional_const = 0.2
+proportional_const = 0.5
 
 # we assume line_color to be darker than plane_color
-line_value = 85
-plane_value = 266
+line_value = 42
+plane_value = 390
 
 # power of both motors when running straight
-target_power = 25
+base_pulses = 200
 
 # web server ip/port
 web_server_ip = '10.42.0.1'
 web_server_port = 5000
+
+# possible ips for robots
+#robot_ips = ['192.168.1.10{}'.format(i) for i in range(1, 5)]
+robot_ips = ['10.42.0.{}'.format(3+i) for i in range(1, 5)]
+# tcp port used for the messaging protocol
+message_port = 31337
+
+# all the possible colors assumed by the nodes in the graph
+Color = Enum('Color', 'red green cyan violet unknown')
+
+saturation_thr = 0.6
--- robot/drs.py
+++ robot/drs.py
@@ -7,13 +7,15 @@ import sys
 import signal
 import zmq
 import conf
-from time import sleep
+import logging
+import atexit
+from time import sleep, time
 from colorsys import rgb_to_hsv
-from urllib2 import urlopen
+from urllib2 import urlopen, URLError
 from collections import deque
-from ev3.ev3dev import Motor
-from ev3.lego import ColorSensor
-from ev3.lego import InfraredSensor
+from threading import Thread
+from enum import Enum
+from ev3dev import *
 
 __authors__ = ["Marco Squarcina <squarcina at dais.unive.it>"]
 __status__  =  "Development"
@@ -22,28 +24,56 @@ __status__  =  "Development"
 # global variables 
 
 # instances of motors
-motor_left = Motor(port = Motor.PORT.D)
-motor_right = Motor(port = Motor.PORT.B)
+motor_left = large_motor(OUTPUT_D)
+motor_right = large_motor(OUTPUT_B)
 # instances of sensors
-color_sensor = ColorSensor()
-ir_sensor  = InfraredSensor()
+col_sensor = color_sensor()
+ir_sensor  = infrared_sensor()
 # other variables
 ir_buffer = [[deque(), deque()] for _ in range(4)]
 ir_medians = [[None, None] for _ in range(4)]
-# zmq context and sockets definitions
+# zmq context definitions
 context = zmq.Context()
-sock_in = context.socket(zmq.REP)
-sock_out = context.socket(zmq.REQ)
+# possible states
+State = Enum('State', ('explore_node_init explore_node explore_edge_init '
+                       'explore_edge_before_marker explore_edge escaping_init '
+                       'escaping waiting_for_clearance moving_init '
+                       'moving_before_marker moving idling '
+                        'in_marker'))
+
 
 # function definitions
 
-def stop(signal = None, frame = None):
-    motor_left.stop()
-    motor_right.stop()
+def message_server():
+    sock = context.socket(zmq.REP)
+    sock.bind("tcp://0.0.0.0:{}".format(conf.message_port))
+    
+    # log incoming messages and reply back
+    # [TODO] define a poison pill to kill this thread
+    while True:
+        message = sock.recv()
+        sock.send("Echoed: [{}]".format(message))
+        logging.info(message)
+
+def reset(signal = None, frame = None):
+    """Stop the motors. [FIXME] is it possible to break?"""
+
+    motor_left.reset()
+    motor_right.reset()
     if signal:
         sys.exit(1)
 
+def stop_motors():
+    motor_left.stop() #pulses_per_second_setpoint = 0
+    motor_right.stop() #pulses_per_second_setpoint = 0
+
+def start_motors():
+    motor_left.run()
+    motor_right.run()
+
 def median(data):
+    """Compute the median of the provided data."""
+
     data = sorted(data)
     n = len(data)
     if n == 0:
@@ -55,58 +85,138 @@ def median(data):
         return (data[i-1] + data[i])/2
 
 def wait_launch():
+    """Wait the game to be started (click play on the web interface) before
+    running."""
+
     url_to_check = "http://{}:{}/started".format(
         conf.web_server_ip, conf.web_server_port)
     started = False
     while not started:
-        f = urlopen(url_to_check)
-        started = True if f.read() == '1' else False
+        try:
+            f = urlopen(url_to_check)
+            started = True if f.read() == '1' else False
+        except URLError:
+            logging.error('Unable to connect to the web server, proceeding')
+            break
         sleep(0.5)
 
+def greet():
+    """Say hello before starting the protocol."""
+
+    # set the second parameter to False for non-blocking call
+    sound.speak("Hello, I am the robot number {}".format(conf.my_robot_id), True)
+
+def follow_line(value, offset, pulses = conf.base_pulses):
+    """Adjust the speed of the two motors to keep up with the line tracking."""
+
+    error = value * 1000 - offset
+    correction = int(conf.proportional_const * error)
+    motor_left.pulses_per_second_setpoint = pulses + correction
+    motor_right.pulses_per_second_setpoint = pulses - correction
+
+def initialize():
+    # explicitly set the color sensor in RGB mode
+    col_sensor.mode = 'RGB-RAW'
+    # explicitly set the infrared sensor in SEEK mode
+    ir_sensor.mode = infrared_sensor.mode_ir_seeker
+    # prepare the motors
+    motor_left.regulation_mode = motor.mode_on
+    motor_right.regulation_mode = motor.mode_on
+    motor_left.run_mode = motor.run_mode_forever
+    motor_right.run_mode = motor.run_mode_forever
+    motor_left.run()
+    motor_right.run()
+
+def get_hsv_colors():
+    """Return the Hue, Saturation, Value triple of the sampled color assuming
+    that the color sensor is in RAW-RGB mode."""
+
+    return rgb_to_hsv(*[col_sensor.value(i)/1022 for i in range(col_sensor.num_values())])
+
+def avoid_collision():
+    # query the ir sensor in SEEK mode to avoid collisions
+    seek = [ir_sensor.value(i) for i in range(ir_sensor.num_values())]
+    for robot_id in range(4):
+        # remove the heads
+        if len(ir_buffer[robot_id][0]) >= conf.n_ir_samples:
+            ir_buffer[robot_id][0].popleft()
+            ir_buffer[robot_id][1].popleft()
+        # update the angle
+        ir_buffer[robot_id][0].append(seek[robot_id*2])
+        # update the distance
+        ir_buffer[robot_id][1].append(abs(seek[robot_id*2+1]))
+        # recompute the median
+        ir_medians[robot_id][0] = median(ir_buffer[robot_id][0]) 
+        ir_medians[robot_id][1] = median(ir_buffer[robot_id][1])
+        
+        if ir_medians[robot_id][1] < 20:
+            # [TODO] handle collisions
+            pass
+
+def in_border(saturation):
+    return saturation > conf.saturation_thr
+
 def main():
-    signal.signal(signal.SIGINT, stop)
-   
-    motor_left.polarity_mode = 'normal'
-    motor_right.polarity_mode = 'normal'
+    # register anti-panic handlers
+    signal.signal(signal.SIGINT, reset)
+    atexit.register(reset)
+
+    # configure how logging should be done
+    logging.basicConfig(level=logging.DEBUG, format='[%(levelname)s] (%(threadName)-10s) %(message)s', )
+
+    # parse command line options
+    if len(sys.argv) > 1 and sys.argv[1] == '--wait':
+        # wait the protocol to be started
+        wait_launch()
+
+    # create a thread for reading incoming zmq messages
+    server = Thread(name='MessageServer', target=message_server)
+    server.setDaemon(True)
+    server.start()
+
+    '''
+    # instance of the socket used for sending messages
+    sock = context.socket(zmq.REQ)
+    logging.info("connecting")
+    sock.connect("tcp://192.168.10.101:{}".format(conf.message_port))
+    logging.info("sending message")
+    sock.send("HALT")
+    logging.info("receiving message")
+    print(sock.recv())
+    '''
 
     # offset repesents the color on the line border. It is simply computed as
     # the average of the value (as in HSV) between the line and the border
     offset = (conf.line_value + conf.plane_value)/2
 
-    # wait the protocol to be started
-    wait_launch()
-
+    greet()
+    initialize()
+    state = State.moving
     while True:
         # query the ir sensor in SEEK mode to avoid collisions
-        seek = ir_sensor.seek
-        for chan in range(4):
-            # remove the heads
-            if len(ir_buffer[chan][0]) >= conf.n_ir_samples:
-                ir_buffer[chan][0].popleft()
-                ir_buffer[chan][1].popleft()
-            # update the angle
-            ir_buffer[chan][0].append(seek[chan][0])
-            # update the distance
-            ir_buffer[chan][1].append(abs(seek[chan][1]))
-            # recompute the median
-            ir_medians[chan][0] = median(ir_buffer[chan][0]) 
-            ir_medians[chan][1] = median(ir_buffer[chan][1])
-            
-        print(ir_medians)
+        avoid_collision()
 
-        # update the speed of each motor using the value of the HSV triple
-        value = rgb_to_hsv(*color_sensor.rgb)[2]
-        error = value - offset
-        turn = conf.proportional_const * error
-        power_left = conf.target_power + turn
-        power_right = conf.target_power - turn
-        try:
-            motor_left.run_forever(power_left, regulation_mode = False)
-            motor_right.run_forever(power_right, regulation_mode = False)
-        except IOError:
-            pass
+        # read the HSV triple from the color sensor and act accordingly to the
+        # protocol
+        hue, saturation, value = get_hsv_colors()
+        if state == State.moving:
+            if in_border(saturation):
+                # found a marker, we need to stop as soon as we find a matching
+                # color
+                state = State.in_marker
+                stop_motors()
+                sound.speak("Oh geez, I found a marker. Now I will go on slowly because it is boring to stay here!", True)
+                start_motors()
+            else:
+                follow_line(value, offset)
+        elif state == State.in_marker:
+            follow_line(value, offset, 80)
+        else:
+            logging.critical("WTF?")
+            break
 
-    stop()
+    reset()
+    # [TODO] join the MessageServer thread
     sys.exit(0)
 
 if __name__ == '__main__':
