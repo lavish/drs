ed37b4aec41ab2e8c8b025c49bc8228584f4f3d5
henry <henry@henryVaio.(none)>
2015-01-25 19:08:57 +0100

    Finished state machine skeleton, fixed Dijkstra for sparse graphs

--- snippets/protocol.py
+++ snippets/protocol.py
@@ -1,12 +1,16 @@
+#more shortage close ordering
+
 from __future__ import division
 
 from time import time, sleep
 
 from enum import Enum
 
+from random import randint
+
 import dijkstra
 
-States = Enum('States', 'explore_node_init explore_node explore_edge_init explore_edge_before_marker explore_edge escaping_init escaping waiting_for_clearance moving_init moving_before_marker moving idling')
+States = Enum('States', 'explore_node_init explore_node explore_edge_init explore_edge_before_marker explore_edge explore_edge_after_marker escaping_init escaping waiting_for_clearance moving_init moving_before_marker moving idling')
 
 global current_state = States.explore_node_init
 
@@ -18,7 +22,7 @@ global current_edge = [Color.red.value, (Color.unknown.value, -1), Direction.n.v
 
 #global unexplored_edges = []
 
-global rotating_mate = None
+global waiting_mate = None
 
 def stop():
     raise Exception('Stop: Not implemented')
@@ -56,6 +60,9 @@ def identfy_node():
 def release_lock(color):
     raise Exception('release_lock: Not implemented')
 
+def release_edge_lock(edge):
+    raise Exception('release_edge_lock: Not implemented')
+
 def lock(color):
     raise Exception('lock: Not implemented')
 
@@ -80,7 +87,7 @@ def get_motor_position():
 def turn_around():
     raise Exception('turn_around: Not implemented')
 
-def notify_clearance():
+def notify_clearance(rotating_mate):
     raise Exception('notify_clearance: Not implemented')
 
 def filter_explored():
@@ -99,98 +106,96 @@ def update():
             current_node = color
             if not explored(color):
                 rotate_in_node()
-                #for edge in get_unexplored_edges(graph, current_node):
+                #for edge in get_unexplored_edges(graph, current_node):zh
                 #    unexplored_edges.append(edge)
             state = States.explore_node
 
         elif state == States.explore_node:
-            unexplored_edges = get_min_available_unexplored(graph, current_node)
-            if len(unexplored_edges) == 0:
+            directions = get_min_dest_direction(graph, current_node)
+            if directions == None:
                 state = States.idling
             else:
-                current_edge = unexplored_edges.pop()
-                if current_edge[0] != current_node:
+                dest = directions[randint(0, len(directions) - 1)]
+                current_edge = (current_node, dest[1], dest[0])
+                if dest[0] != Color.unknown.value:
                     state = States.moving_init
                 else:
-                    if not is_edge_locked(current_edge):
-                        state = States.explore_edge_init
+                    state = States.explore_edge_init
 
         elif state == States.explore_edge_init:
             outupdate() # with direction or lock on edges
-            move_to_edge(current_edge)
+            move_to_edge(current_edge[1])
             state = States.explore_edge_before_marker
+            #START!!!
 
         elif state == States.explore_edge_before_marker:
+            seen_robots = get_seen_robots()
+            if len(seen_robots) > 0:
+                solve_collision(seen_robots, current_edge, -1)
+                state = States.waiting_for_clearance # corrosive husking candling pathos
             if on_marker():
                 stop()
                 color = identfy_marker()
-                release_lock(color)
+                assert (color == current_node), "Wrong marker found... Colors do not match..."
                 reset_motor_position()
                 state = States.explore_edge
 
         elif state == States.explore_edge:
-            seen_robots = get_seen_robots()
-            if not right_of_way(seen_robots):
+            seen_robots = get_seen_robots() #maybe replace returned list with None or element: more shortage close ordering
+            if len(seen_robots) > 0:
+                solve_collision(seen_robots, current_edge, get_motor_position())
+                waiting_mate = seen_robots[0] # since there can only be one
                 state = States.escaping_init
-            elif len(seen_robots) > 0 and right_of_way(seen_robots):
-                state = States.waiting_for_clearance 
-                rotating_mate = seen_robots[0] # since there can only be one
-                # I'm not saving old state because waiting for clearance can only happen on exploring, since moving has the lowest priority on edges
-            if on_node():
-                state = States.explore_node_init
             elif on_marker():
                 stop()
+                edge_length = get_motor_position()
                 marker_color = identfy_marker()
+                orientation = get_orientation()
+                edge_update(current_edge[0], current_edge[2], marker_color, orientation, edge_length)
                 if is_locked(marker_color):
-                    edge_length = get_motor_position()
-                    edge_update(current_edge[0], (marker_color, edge_length), current_edge[2])
+                    rotating_mate = None
                     state = States.escaping_init
                 else:
-                    edge_length = get_motor_position()
-                    orientation = get_orientation()
-                    edge_update(current_edge[0], current_edge[2], marker_color, orientation, edge_length)
                     lock(color)
+                    release_lock(current_node)
+                    current_node = color
+                    state = States.explore_edge_after_marker
+
+        elif state == States.explore_edge_after_marker:             
+            if on_node():
+                release_edge_lock(cuurent_edge)
+                state = States.explore_node_init
 
         elif state == States.escaping_init:
-            turn_around() # check marker lock
-            notify_clearance()
-            state = States.escaping
+            found_marker = turn_around() # check marker
+            if waiting_mate != None:
+                notify_clearance(waiting_mate)
+            if found_marker:
+                state = States.explore_edge_after_marker
+            else:
+                state = States.escaping
 
         elif state == States.escaping:
-            seen_robots = get_seen_robots()
-            if on_node():
-                state = States.explore_node_init
-            elif on_marker():
+            if on_marker():
                 stop()
                 marker_color = identfy_marker()
-                if is_locked(marker_color):
-                    state = escaping_init
-                else:
-                    lock(color)
+                state = explore_edge_after_marker
 
         elif state == States.moving_init:
-            available_graph = filter_locked(graph)
-            next = get_next_direction(available_graph, current_node, current_edge[0])
-            if next != None:
-                state = States.moving
-                outupdate() # with direction or lock on edges
-                move_to_edge(current_edge)
-                state = States.moving_before_marker
-            else:
-                get_other_destination()
+            outupdate() # with direction or lock on edges
+            move_to_edge(current_edge[1])
+            outupdate() # with direction or lock on edges
+            move_to_edge(current_edge)
+            state = States.moving_before_marker
 
         elif state == States.moving_before_marker:
             if on_marker():
                 stop()
                 color = identfy_marker()
-                release_lock(color)
+                #release_lock(color)
                 state = States.moving
 
         elif state == States.moving:
-            seen_robots = get_seen_robots()
-            if len(seen_robots) > 0:
-                unexplored_edges.append(current_edge) #source of possibles deadlocks... Maybe enqueue
-                state = State.escaping_init
             if on_node():
                 unexplored_edges.append(current_edge) #safe, no deadlocks
                 state = States.explore_node_init
@@ -198,8 +203,26 @@ def update():
                 stop()
                 marker_color = identfy_marker()
                 if is_locked(marker_color):
-                    unexplored_edges.append(current_edge) #source of possibles deadlocks... Maybe enqueue
                     state = States.escaping_init
-                #missing
+                else
+                    lock(marker_color)
+                    release_lock(current_node)
+                    current_node = marker_color
+                    state = States.explore_edge_after_marker
+
+        elif state == States.waiting_for_clearance:
+            #response = check_messages()
+            #if response:
+            #    state = States.explore_edge_before_marker
+            #else:
+            sleep(5) # the time needed for rotation of the mate
+            state = States.explore_edge_before_marker
+
+        elif state == States.idling:
+            sleep(5)
+            state = States.explore_node
+
+        else:
+            raise Exception("Undefined state...")
 
 
